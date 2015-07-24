e230309e114d2b0f78e18991bea648c86ad1583a
Naresh Tanniru <ntanniru@codeaurora.org>
2015-07-24 11:19:31 +0530

    audiopolicy: make audio policy extensible
    make function virtual or protected so that
    they can be extended in custom audio policy.
    Change-Id: Ida7992f6b327491fab1f4ea376e85e8eb34b89ca
    
    audiopolicy: update APM to use custom audio policy configuration
    Change-Id: I3161ff4fa41d37d0b761202f0b3a490d9c7e418e
    
    audiopolicy: allow dp device selection for voice usecases
    Change-Id: I13f1ddd0fd3655376d28a0ebe028d495fa2c6e5f
    
    audiopolicy: follow-up change to support extended feature
    Change-Id: Iddc14a3e2e61bc57f8637eae71e36cc21ce2d3e8
    
    [dianlujitao: Port to R]
    
    Change-Id: Ida7992f6b327491fab1f4ea376e85e8eb34b89ca
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- services/audiopolicy/managerdefault/AudioPolicyManager.h
+++ services/audiopolicy/managerdefault/AudioPolicyManager.h
@@ -539,7 +539,7 @@ protected:
         // when a device is disconnected, checks if an output is not used any more and
         // returns its handle if any.
         // transfers the audio tracks and effects from one output thread to another accordingly.
-        status_t checkOutputsForDevice(const sp<DeviceDescriptor>& device,
+        virtual status_t checkOutputsForDevice(const sp<DeviceDescriptor>& device,
                                        audio_policy_dev_state_t state,
                                        SortedVector<audio_io_handle_t>& outputs);
 
@@ -756,10 +756,10 @@ protected:
         DeviceVector selectBestRxSinkDevicesForCall(bool fromCache);
         bool isDeviceOfModule(const sp<DeviceDescriptor>& devDesc, const char *moduleId) const;
 
-        status_t startSource(const sp<SwAudioOutputDescriptor>& outputDesc,
+        virtual status_t startSource(const sp<SwAudioOutputDescriptor>& outputDesc,
                              const sp<TrackClientDescriptor>& client,
                              uint32_t *delayMs);
-        status_t stopSource(const sp<SwAudioOutputDescriptor>& outputDesc,
+        virtual status_t stopSource(const sp<SwAudioOutputDescriptor>& outputDesc,
                             const sp<TrackClientDescriptor>& client);
 
         void clearAudioPatches(uid_t uid);
@@ -888,7 +888,7 @@ protected:
         PatchBuilder buildMsdPatch(bool msdIsSource, const sp<DeviceDescriptor> &device) const;
         status_t setMsdOutputPatches(const DeviceVector *outputDevices = nullptr);
         void releaseMsdOutputPatches(const DeviceVector& devices);
-private:
+protected:
         void onNewAudioModulesAvailableInt(DeviceVector *newDevices);
 
         // Add or remove AC3 DTS encodings based on user preferences.
@@ -930,7 +930,7 @@ private:
                 std::vector<sp<AudioPolicyMix>> *secondaryMixes,
                 output_type_t *outputType);
         // internal method to return the output handle for the given device and format
-        audio_io_handle_t getOutputForDevices(
+        virtual audio_io_handle_t getOutputForDevices(
                 const DeviceVector &devices,
                 audio_session_t session,
                 audio_stream_type_t stream,
@@ -975,7 +975,7 @@ private:
         bool     isValidAttributes(const audio_attributes_t *paa);
 
         // Called by setDeviceConnectionState().
-        status_t setDeviceConnectionStateInt(audio_devices_t deviceType,
+        virtual status_t setDeviceConnectionStateInt(audio_devices_t deviceType,
                                              audio_policy_dev_state_t state,
                                              const char *device_address,
                                              const char *device_name,

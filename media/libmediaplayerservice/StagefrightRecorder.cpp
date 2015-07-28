3963c039d13ecd0f1d259abfae6c11958f850aa7
Praveen Chavan <pchavan@codeaurora.org>
2015-07-27 20:01:35 -0700

    Stagefright: Do not skip frames in time-lapse-source for high-speed
    Avoid the frame-skip logic in time-lapse source if capture-rate
    exceeds video-fps. Not doing so will drop frames and also cause
    retrograde timestamps and an assertion in CameraSource.
    
    Also, calculate the TotalPausedDuration value considering the
    capturerate and framerate
    
    Change-Id: I8420e44ab96484f0d6301c366a24eefc8efeaf0f
    
    [Kshitij Gupta]
     * Adapt for 8.0.0 by using resumeStartTimeUs instead
        of (systemTime() / 1000)
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- media/libmediaplayerservice/StagefrightRecorder.cpp
+++ media/libmediaplayerservice/StagefrightRecorder.cpp
@@ -2030,9 +2030,12 @@ status_t StagefrightRecorder::resume() {
         if (mPauseStartTimeUs < bufferStartTimeUs) {
             mPauseStartTimeUs = bufferStartTimeUs;
         }
-        // 30 ms buffer to avoid timestamp overlap
-        mTotalPausedDurationUs += resumeStartTimeUs - mPauseStartTimeUs - 30000;
-    }
+
+    // 30 ms buffer to avoid timestamp overlap
+    mTotalPausedDurationUs +=
+        resumeStartTimeUs - mPauseStartTimeUs -
+        (30000 * (mCaptureFpsEnable ? (mCaptureFps / mFrameRate):1));
+      }
     double timeOffset = -mTotalPausedDurationUs;
     if (mCaptureFpsEnable && (mVideoSource == VIDEO_SOURCE_CAMERA)) {
         timeOffset *= mCaptureFps / mFrameRate;

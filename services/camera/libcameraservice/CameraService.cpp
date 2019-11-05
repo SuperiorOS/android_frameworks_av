4906f6e6d2e93b9ae034556ec40310850b42236b
darkobas <darkobas@gmail.com>
2019-11-05 19:03:47 +0100

    cameraservice: move client_package_name to data/misc
    Change-Id: I0d244b81625fd9514000bea313a28ad60321333f
    Signed-off-by: Justin Crawford <Justin@stacksmash.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -3008,7 +3008,7 @@ status_t CameraService::BasicClient::startCameraOps() {
     sCameraService->updateOpenCloseStatus(mCameraIdStr, true/*open*/, mClientPackageName);
 
 #ifdef TARGET_NEEDS_CLIENT_INFO
-    std::ofstream cpf("/data/vendor/aosp/client_package_name");
+    std::ofstream cpf("/data/misc/aosp/client_package_name");
     std::string cpn = String8(mClientPackageName).string();
     cpf << cpn;
 #endif

1171309d043581ac2995719fa8923c21f20ec3c0
Justin Crawford <Justin@stacksmash.net>
2020-12-18 03:57:37 -0800

    cameraservice: rename aosp to superior for client_package_name
    Signed-off-by: Justin Crawford <Justin@stacksmash.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Change-Id: I133a8bffc20c5c526d6dc9eab0b1ccc360a61299

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -3008,8 +3008,8 @@ status_t CameraService::BasicClient::startCameraOps() {
     sCameraService->updateOpenCloseStatus(mCameraIdStr, true/*open*/, mClientPackageName);
 
 #ifdef TARGET_NEEDS_CLIENT_INFO
-    std::ofstream cpf("/data/misc/aosp/client_package_name");
-    std::string cpn = String8(mClientPackageName).string();
+	std::ofstream cpf("/data/misc/superior/client_package_name");
+	std::string cpn = String8(mClientPackageName).string();
     cpf << cpn;
 #endif
 
